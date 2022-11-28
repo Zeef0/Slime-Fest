f0520b0b31f3637fb06951e34be59287b9f019b4
Zeef0 <79386646+Zeef0@users.noreply.github.com>
2022-11-28 19:02:46 +0800

    Created sprite class for slime

--- main.py
+++ main.py
@@ -3,16 +3,32 @@ from pygame.locals import *
 import sys
 
 
+class Slime(pygame.sprite.Sprite):
+    def __init__(self, image_path):
+        super().__init__()
+        self.image = pygame.image.load(image_path)
+        self.rect = self.image.get_rect()
+
+    def update(self):
+        self.rect.center = pygame.mouse.get_pos()
+
+
 pygame.init()
 
 w, h = 600, 400
 screen = pygame.display.set_mode((w, h))
 pygame.display.set_caption("Slime Fest")
 
+slime = Slime("./assets/slime.png")
+slime_group = pygame.sprite.Group()
+slime_group.add(slime)
+
 
 def draw():
     pygame.display.update()
     screen.fill("Pink")
+    slime_group.draw(screen)
+    slime.update()
 
 
 def main():
