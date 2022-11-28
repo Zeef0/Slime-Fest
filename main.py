7451b5daf7fb7b9197763ff45257569a51fb392c
Zeef0 <79386646+Zeef0@users.noreply.github.com>
2022-11-28 10:58:20 +0800

    Finished setting up pygame

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
 import pygame
+from pygame.locals import *
+import sys
+
 
 pygame.init()
+
+w, h = 600, 400
+screen = pygame.display.set_mode((w, h))
+pygame.display.set_caption("Slime Fest")
+
+
+def draw():
+    pygame.display.update()
+    screen.fill("Pink")
+
+
+def main():
+    game_active = True
+    while game_active:
+        draw()
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+
+
+if __name__ == "__main__":
+    main()
